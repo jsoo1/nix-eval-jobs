0d651f6bae41cf79248937abd3a441487bdf8ae2
John Soo <john.soo@arista.com>
2022-03-16 18:11:31 -0700

    Replace getDerivation with getDerivations.
    To account for nixos systems and other "derivation-like" buildables.

--- src/nix-eval-jobs.cc
+++ src/nix-eval-jobs.cc
@@ -123,6 +123,12 @@ static void showPath_(std::vector<std::string> & path, std::ostringstream & dest
     }
 }
 
+static nlohmann::json response(std::string & attrName) {
+    nlohmann::json reply;
+    reply["attr"] = attrName;
+    return reply;
+}
+
 static void worker(
     EvalState & state,
     Bindings & autoArgs,
@@ -156,10 +162,10 @@ static void worker(
         }
 
         /* Evaluate it and send info back to the master. */
-        nlohmann::json reply;
+
         std::ostringstream attrNameS;
         showPath_(attrPath, attrNameS);
-        reply["attr"] = attrNameS.str();
+        auto attrName = attrNameS.str();
 
         try {
             auto v = state.allocValue();
@@ -177,9 +183,9 @@ static void worker(
             attrVal = v;
 
             for (auto attrName : attrPath) {
-                auto a = attrVal->attrs->get(state.symbols.create(attrName));
+                auto a = attrVal->attrs->find(state.symbols.create(attrName));
 
-                if (!a) {
+                if (a != attrVal->attrs->end()) {
                     std::ostringstream oss;
                     showPath_(attrPath, oss);
                     throw Error("attribute '%s' not found along path %s", attrName, oss.str());
@@ -195,43 +201,51 @@ static void worker(
             }
 
 
+            DrvInfos drvs;
+            getDerivations(state, *attrVal, "", autoArgs, drvs, false);
 
-            if (auto drv = getDerivation(state, *attrVal, false)) {
-                std::string system = drv->querySystem();
+            if (!drvs.empty()) {
+                for (auto drv : drvs) {
+                    std::string system = drv.querySystem();
 
-                if (system == "unknown")
-                    throw EvalError("derivation must not have unknown system type");
+                    if (system == "unknown")
+                        throw EvalError("derivation must not have unknown system type");
 
-                auto drvPath = drv->queryDrvPath();
-                auto outputs = drv->queryOutputs(false);
+                    auto drvPath = drv.queryDrvPath();
+                    auto outputs = drv.queryOutputs(false);
 
-                reply["name"] = drv->queryName();
-                reply["system"] = system;
-                reply["drvPath"] = drvPath;
-                for (auto out : outputs){
-                    reply["outputs"][out.first] = out.second;
-                }
+                    auto reply = response(attrName);
 
-                if (myArgs.meta) {
-                    nlohmann::json meta;
-                    for (auto & name : drv->queryMetaNames()) {
-                      PathSet context;
-                      std::stringstream ss;
-
-                      auto metaValue = drv->queryMeta(name);
-                      // Skip non-serialisable types
-                      // TODO: Fix serialisation of derivations to store paths
-                      if (metaValue == 0) {
-                        continue;
-                      }
-
-                      printValueAsJSON(state, true, *metaValue, ss, context);
-                      nlohmann::json field = nlohmann::json::parse(ss.str());
-                      meta[name] = field;
+                    reply["name"] = drv.queryName();
+                    reply["system"] = system;
+                    reply["drvPath"] = drvPath;
+                    for (auto out : outputs){
+                        reply["outputs"][out.first] = out.second;
                     }
-                    reply["meta"] = meta;
-                }
 
+                    if (myArgs.meta) {
+                        nlohmann::json meta;
+                        for (auto & name : drv.queryMetaNames()) {
+                          PathSet context;
+                          std::stringstream ss;
+
+                          auto metaValue = drv.queryMeta(name);
+                          // Skip non-serialisable types
+                          // TODO: Fix serialisation of derivations to store paths
+                          if (metaValue == 0) {
+                            continue;
+                          }
+
+                          printValueAsJSON(state, true, *metaValue, ss, context);
+                          nlohmann::json field = nlohmann::json::parse(ss.str());
+                          meta[name] = field;
+                        }
+                        reply["meta"] = meta;
+                    }
+
+                    writeLine(to.get(), reply.dump());
+
+                }
             }
 
             else if (attrVal->type == tAttrs)
@@ -247,7 +261,10 @@ static void worker(
 
                     paths.push_back(attrs);
                 }
+                auto reply = response(attrName);
                 reply["attrs"] = std::move(paths);
+
+                writeLine(to.get(), reply.dump());
             }
 
             else if (attrVal->type == tNull)
@@ -260,15 +277,17 @@ static void worker(
             }
 
         } catch (EvalError & e) {
+            auto reply = response(attrName);
+
             // Transmits the error we got from the previous evaluation
             // in the JSON output.
             reply["error"] = filterANSIEscapes(e.msg(), true);
             // Don't forget to print it into the STDERR log, this is
             // what's shown in the Hydra UI.
             printError(e.msg());
-        }
 
-        writeLine(to.get(), reply.dump());
+            writeLine(to.get(), reply.dump());
+        }
 
         /* If our RSS exceeds the maximum, exit. The master will
            start a new process. */
@@ -371,7 +390,9 @@ int main(int argc, char * * argv)
                         continue;
                     } else if (s != "next") {
                         auto json = nlohmann::json::parse(s);
-                        throw Error("worker error: %s", (std::string) json["error"]);
+
+                        if (json.find("error") != json.end())
+                            throw Error("worker error: %s", (std::string) json["error"]);
                     }
 
                     /* Wait for a job name to become available. */
