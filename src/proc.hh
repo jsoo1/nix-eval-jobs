9c8e8d273f6f546b6ec884c4339d8ca9d709d95e
John Soo <john.soo@arista.com>
2022-04-27 22:49:18 -0700

    Move Proc to a separate file.

---
+++ src/proc.hh
@@ -0,0 +1,23 @@
+#include <nix/util.hh>
+#include <nix/eval.hh>
+
+using namespace nix;
+
+namespace nix_eval_jobs {
+
+struct MyArgs;
+
+typedef std::function<void(MyArgs & myArgs, EvalState & state,
+                           Bindings & autoArgs, AutoCloseFD & to,
+                           AutoCloseFD & from)>
+    Processor;
+
+/* Auto-cleanup of fork's process and fds. */
+struct Proc {
+    AutoCloseFD to, from;
+    Pid pid;
+    Proc(MyArgs & myArgs, const Processor & proc);
+    ~Proc() { }
+};
+
+}
