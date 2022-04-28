9e3c7869c9de25fd889fa4b6dcf27d3e59cab404
John Soo <john.soo@arista.com>
2022-04-27 22:48:53 -0700

    Make Job and evaluation result types to model jobs.

---
+++ src/job.cc
@@ -0,0 +1,204 @@
+#include <nix/config.h>
+#include <nix/eval.hh>
+#include <nix/get-drvs.hh>
+#include <nix/globals.hh>
+#include <nix/local-fs-store.hh>
+#include <nix/shared.hh>
+#include <nix/value-to-json.hh>
+
+#include <nlohmann/json.hpp>
+
+#include "args.hh"
+#include "accessor.hh"
+#include "job.hh"
+
+using json = nlohmann::json;
+using namespace nix;
+
+namespace nix_eval_jobs {
+
+/* Job */
+
+Drvs::Drvs(MyArgs & myArgs, EvalState & state, Bindings & autoArgs, Value & v) {
+    DrvInfos drvInfos;
+    getDerivations(state, v, "", autoArgs, drvInfos, false);
+
+    for (auto & drvInfo : drvInfos)
+        this->drvs.push_back(Drv(myArgs, state, drvInfo));
+
+}
+
+Drvs::Drvs(const Drvs & that) {
+    this->drvs = that.drvs;
+}
+
+Drv::Drv(MyArgs & myArgs, EvalState & state, DrvInfo & drvInfo) {
+    if (drvInfo.querySystem() == "unknown")
+        throw EvalError("derivation must have a 'system' attribute");
+
+    auto localStore = state.store.dynamic_pointer_cast<LocalFSStore>();
+
+    for (auto out : drvInfo.queryOutputs(true)) {
+        if (out.second)
+            this->outputs[out.first] = localStore->printStorePath(*out.second);
+
+    }
+
+    if (myArgs.meta) {
+        json meta_;
+        for (auto & name : drvInfo.queryMetaNames()) {
+            PathSet context;
+            std::stringstream ss;
+
+            auto metaValue = drvInfo.queryMeta(name);
+            // Skip non-serialisable types
+            // TODO: Fix serialisation of derivations to store paths
+            if (metaValue == 0) {
+                continue;
+            }
+
+            printValueAsJSON(state, true, *metaValue, noPos, ss, context);
+
+            meta_[name] = json::parse(ss.str());
+        }
+        this->meta = meta_;
+    }
+
+    this->name = drvInfo.queryName();
+    this->system = drvInfo.querySystem();
+    this->drvPath = localStore->printStorePath(drvInfo.requireDrvPath());
+
+}
+
+JobAttrs::JobAttrs(EvalState & state, Bindings & autoArgs, Value & vIn) {
+    v = state.allocValue();
+    state.autoCallFunction(autoArgs, vIn, *v);
+
+    if (v->type() != nAttrs)
+        throw TypeError("wanted a JobAttrs, got %s", showType(vIn));
+}
+
+JobList::JobList(EvalState & state, Bindings & autoArgs, Value & vIn) {
+    v = state.allocValue();
+    state.autoCallFunction(autoArgs, vIn, *v);
+
+    if (v->type() != nList)
+        throw TypeError("wanted a JobList, got %s", showType(vIn));
+}
+
+/* children : HasChildren -> vector<Accessor> */
+
+std::vector<std::unique_ptr<Accessor>> JobAttrs::children() {
+    std::vector<std::unique_ptr<Accessor>> children;
+
+    for (auto & a : this->v->attrs->lexicographicOrder())
+        children.push_back(std::make_unique<Name>(a->name));
+
+    return children;
+}
+
+std::vector<std::unique_ptr<Accessor>> JobList::children() {
+    std::vector<std::unique_ptr<Accessor>> children;
+    unsigned long i = 0;
+
+    #ifdef __GNUC__
+    #pragma GCC diagnostic ignored "-Wunused-variable"
+    #elif __clang__
+    #pragma clang diagnostic ignored "-Wunused-variable"
+    #endif
+    for (auto & _ : v->listItems())
+        children.push_back(std::make_unique<Index>(i++));
+    #ifdef __GNUC__
+    #pragma GCC diagnostic warning "-Wunused-variable"
+    #elif __clang__
+    #pragma clang diagnostic warning "-Wunused-variable"
+    #endif
+
+    return children;
+}
+
+/* eval : Job -> EvalState -> JobEvalResult */
+
+JobEvalResults Drvs::eval(MyArgs & myArgs, EvalState & state) {
+    /* Register the derivation as a GC root.  !!! This
+       registers roots for jobs that we may have already
+       done. */
+    if (myArgs.gcRootsDir != "") {
+        auto localStore = state.store.dynamic_pointer_cast<LocalFSStore>();
+
+        for (auto & drv : this->drvs) {
+            auto storePath = localStore->parseStorePath(drv.drvPath);
+            Path root = myArgs.gcRootsDir + "/" + std::string(baseNameOf(drv.drvPath));
+            if (!pathExists(root)) {
+                localStore->addPermRoot(storePath, root);
+            }
+        }
+    }
+
+    JobEvalResults res;
+    for (auto & d : this->drvs)
+        res.push_back(d.clone());
+
+    return res;
+}
+
+JobEvalResults JobAttrs::eval(MyArgs & myArgs, EvalState & state) {
+    JobEvalResults res;
+    res.push_back(JobChildren(*this).clone());
+    return res;
+}
+
+JobEvalResults JobList::eval(MyArgs & myArgs, EvalState & state) {
+    JobEvalResults res;
+    res.push_back(JobChildren(*this).clone());
+    return res;
+}
+
+/* JobEvalResult */
+
+JobChildren::JobChildren(HasChildren & parent) {
+    this->children = parent.children();
+}
+
+JobChildren::JobChildren(const JobChildren & that) {
+    for (auto & p : that.children)
+        this->children.push_back(p->clone());
+}
+
+/* clone : JobChildren -> JobChildren */
+
+std::unique_ptr<JobEvalResult> Drv::clone() {
+    return std::make_unique<Drv>(*this);
+}
+
+std::unique_ptr<JobEvalResult> JobChildren::clone() {
+    return std::make_unique<JobChildren>(*this);
+}
+
+/* toJson : JobEvalResult -> json */
+
+json JobChildren::toJson() {
+    std::vector<json> children;
+
+    for (auto & child : this->children)
+        children.push_back(child->toJson());
+
+    return json{ {"children", children } };
+
+}
+
+json Drv::toJson() {
+    json j;
+
+    j["name"]  = this->name;
+    j["system"] = this->system;
+    j["drvPath"] = this->drvPath;
+    j["outputs"] = this->outputs;
+
+    if (this->meta.has_value())
+        j["meta"] = this->meta.value();
+
+    return j;
+}
+
+}
---
+++ src/job.hh
@@ -0,0 +1,153 @@
+#include <nix/eval.hh>
+#include <nix/get-drvs.hh>
+#include <nix/local-fs-store.hh>
+#include <nix/value-to-json.hh>
+
+#include <nlohmann/json.hpp>
+#include "accessor.hh"
+
+using json = nlohmann::json;
+using namespace nix;
+
+namespace nix_eval_jobs {
+
+class Accessor;
+struct AccessorPath;
+struct MyArgs;
+
+/* JobEvalResult := JobChildren (Vector Accessor) | Drvs
+
+   What you get from evaluating a Job. Might be more children to
+   evaluate or leaf Drvs.
+ */
+class JobEvalResult {
+public:
+    /* toJson : JobEvalResult -> json */
+    virtual json toJson() = 0;
+    /* clone : JobEvalResult -> JobEvalResult */
+    virtual std::unique_ptr<JobEvalResult> clone() = 0;
+    virtual ~JobEvalResult() { };
+};
+
+typedef std::vector<std::unique_ptr<JobEvalResult>> JobEvalResults;
+
+/* Job := Drvs | JobAttrs | JobList
+
+   Drvs := vector Drv
+
+   JobAttrs := Attrs Job
+
+   JobList := List Job
+
+   The types of expressions nix-eval-jobs can evaluate
+
+   The implementation (i.e. with JobChildren as children) is a
+   different than the grammar because of the way AccessorPath is used
+   to walk Jobs.
+
+   There may be multiple Drv because of `recurseForDerivations`.
+
+   Create one with `getJob` or by traversing a Value with
+   `AccessorPath::walk`.
+
+   Use it by `eval`ing it.
+ */
+class Job {
+public:
+    /* eval : Job -> EvalState -> vector JobEvalResult */
+    virtual JobEvalResults eval(MyArgs & myArgs, EvalState & state) = 0;
+    virtual ~Job() { };
+};
+
+/* a plain drv - (almost) the primitive for nix-eval-jobs */
+struct Drv : JobEvalResult {
+    std::string name;
+    std::string system;
+    std::string drvPath;
+    std::map<std::string, std::string> outputs;
+    std::optional<json> meta;
+    Drv(MyArgs & myArgs, EvalState & state, DrvInfo & drvInfo);
+    Drv(const Drv & that) = default;
+    std::unique_ptr<JobEvalResult> clone() override;
+    json toJson() override;
+    ~Drv() { };
+};
+
+/* The leaf on the tree of derivations (there may be multiple due to
+   `recurseForDerivations`)
+ */
+struct Drvs : Job {
+    std::vector<Drv> drvs;
+    Drvs(MyArgs & myArgs, EvalState & state, Bindings & autoArgs, Value & v);
+    Drvs(const Drvs & that);
+    JobEvalResults eval(MyArgs & myArgs, EvalState & state) override;
+    ~Drvs() { };
+};
+
+/* which Jobs are collections */
+class HasChildren {
+public:
+    virtual std::vector<std::unique_ptr<Accessor>> children() = 0;
+    virtual ~HasChildren() { };
+};
+
+/* The forest Jobs when Job is a collection
+
+   Get one by `eval`ing a Job.
+ */
+struct JobChildren : JobEvalResult {
+    std::vector<std::unique_ptr<Accessor>> children;
+    JobChildren(HasChildren & children);
+    JobChildren(const JobChildren & that);
+    std::unique_ptr<JobEvalResult> clone() override;
+    json toJson() override;
+    ~JobChildren() { };
+};
+
+/* An attrset of Job */
+struct JobAttrs : Job, HasChildren {
+    Value * v;
+    JobAttrs(EvalState & state, Bindings & autoArgs, Value & vIn);
+    std::vector<std::unique_ptr<Accessor>> children() override;
+    JobEvalResults eval(MyArgs & myArgs, EvalState & state) override;
+    ~JobAttrs() { };
+};
+
+/* A list of Job */
+struct JobList : Job, HasChildren {
+    Value * v;
+    JobList(EvalState & state, Bindings & autoArgs, Value & vIn);
+    std::vector<std::unique_ptr<Accessor>> children() override;
+    JobEvalResults eval(MyArgs & myArgs, EvalState & state) override;
+    ~JobList() { };
+};
+
+/* Parse a Job from a nix value */
+/* Ignore unused function warnings, as it is actually used  in `main` */
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-function"
+#elif __clang__
+#pragma clang diagnostic ignored "-Wunused-function"
+#endif
+static std::unique_ptr<Job> getJob(MyArgs & myArgs, EvalState & state, Bindings & autoArgs, Value & v) {
+    try {
+        return std::make_unique<Drvs>(myArgs, state, autoArgs, v);
+    } catch (TypeError & _) {
+        try {
+            return std::make_unique<JobAttrs>(state, autoArgs, v);
+        } catch (TypeError & _) {
+            try {
+                return std::make_unique<JobList>(state, autoArgs, v);
+            } catch (TypeError & _) {
+                throw TypeError("error creating job, expecting one of a derivation, an attrset or a derivation, got: %s", showType(v));
+            }
+        }
+    }
+}
+#ifdef __GNUC__
+#pragma GCC diagnostic warning "-Wunused-function"
+#elif __clang__
+#pragma clang diagnostic warning "-Wunused-function"
+#endif
+
+}
